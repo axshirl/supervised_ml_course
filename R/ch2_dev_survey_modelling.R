6cd522bb2ca1355c12d4ee4ec62e489e33df91a8
axshirl <axshirl@gmail.com>
2021-04-02 02:59:02 -0500

    progress on ch2. EDA, goal is to classify remote work status based on other metrics from respondents

--- R/ch2_dev_survey_modelling.R
+++ R/ch2_dev_survey_modelling.R
@@ -1,10 +1,6 @@
 #### Chapter 2 ####
-
 #stackoverflow dev survey
-#notable mention about the data- 
-#we're trying to model around remote work status
-#but there's roughly 90% of the data in the Not Remote category
-#& this class imbalance will have to be corrected for
+
 
 #we're trying to predict whether a dev is remote or not
 #based on characteristics from their company
@@ -12,3 +8,48 @@
 
 library(tidyverse)
 stack_overflow <- read_csv("data/stack_overflow.csv")
+
+#looking at the data
+glimpse(stack_overflow)
+View(stack_overflow)
+#rough overview of cols
+#respondent num (numeric, looks like index of respondents)
+#country (chr, self explanatory. looks clean already)
+#salary (float, yearly salary of the respondent)
+#years_coded_job (years at a coding job?)
+#open_source (bool, does the respondent contribute to open source projects?)
+#hobby (bool, does respondent code as hobby?)
+#company_size_number (numeric, self explan)
+#remote (character, "Not Remote" or "Remote" NOTE CHAR, NOT BOOL. 
+#Are there other values of the remote field?)
+unique(stack_overflow$remote)
+#Nope. Just these two.
+#career_satisfaction (numeric, 1-10 satisfaction)
+#wonder how salary and remote status affect this tbh. might be a neat
+#regression
+#and the rest are booleans for if they fit into specific job titles
+#i.e. data_scientist == TRUE or web_developer == TRUE
+
+stack_overflow %>%
+  count(remote, sort = TRUE)
+
+#~6300 Non-Remote workers (bet this changes big this year!)
+#and ~700 Remote workers. Note the large class imbalance.
+
+stack_overflow %>%
+  count(country, sort=TRUE)
+
+ggplot(stack_overflow, aes(remote, years_coded_job)) +
+  geom_boxplot() +
+  labs(x = NULL,
+       y = "Years of professional coding experience")
+#Cool- Boxplot showing years of coding experience for each group
+
+#What have we learned?
+#First- Generally our respondents are not remote workers and generally
+#the few that are remote workers have more experience on average
+#as well as most of the respondents are American, making up nearly
+#half of the respondents.
+
+
+
